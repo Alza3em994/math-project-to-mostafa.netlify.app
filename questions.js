359ca54ebf7a3117b4fd73fc1620492b92d8272a
Alza3em994 <114918843+Alza3em994@users.noreply.github.com>
2023-02-08 16:24:21 +0400

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Factor the following expression: x^2 + 10x + 25",
+    answer: "(x + 5)^2",
+    options: [
+      "(x - 6)(x + 6)",
+      "(x + 5)^2",
+      "(x - 5)(x + 5)",
+      "(x - 3)(x + 3)"
+    ]
+  },
+    {
+    numb: 2,
+    question: "x^2 + 6x + 8",
+    answer: "(x + 2)(x + 4)",
+    options: [
+      "(x + 7)^2",
+      "(x - 6)(x + 6)",
+      "(x - 2)(x + 3)",
+      "(x + 2)(x + 4)"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Factor the following expression: x^2 + 14x + 49",
+    answer: "(x + 7)^2",
+    options: [
+      "(x - 3)(x + 3)",
+      "(x - 6)(x + 6)",
+      "(x + 7)^2",
+      "(x + 2)(x + 4)"
+    ]
+  },
+    {
+    numb: 4,
+    question: " 2x^2 + 5x + 2",
+    answer: "(2x + 1)(x + 2)",
+    options: [
+      " (2x + 3)(3x + 3)",
+      "(2x + 1)^2",
+      "(x - 5)(x + 5)",
+      "(2x + 1)(x + 2)"
+    ]
+  },
+    {
+    numb: 5,
+    question: "9x^2 - 81x + 81",
+    answer: " (9x - 9)(x - 9)",
+    options: [
+      " (9x - 9)(x - 9)",
+      "(7x - 1)(x - 7)",
+      "(x - 3)(x + 3)",
+      "(x + 5)^2"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
